21dde1711bbeb6a11b0c6ef3bf9a7958d20fe4a7
MaazWebDev <maazkhan66376@gmail.com>
2024-09-26 22:58:36 +0500

    first commit

---
+++ src/Edit.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { edit } from "./redux/reducers/todoSlice";
+import { useDispatch } from "react-redux";
+
+function Edit({ setEditMode, index, value }) {
+  const dispatch = useDispatch();
+  const [editVal, setEditVal] = useState(value);
+  const editTodo = () => {
+    dispatch(
+      edit({
+        index,
+        editVal,
+      })
+    );
+    setEditMode(false);
+  };
+  return (
+    <div className="flex flex-col sm:flex-row gap-2">
+      <input
+        value={editVal}
+        type="text"
+        className="p-2 input input-bordered input-primary w-full max-w-xs"
+        onChange={(e) => setEditVal(e.target.value)}
+      />
+      <button className="btn btn-accent  ml-0 sm:ml-3" onClick={editTodo}>
+        update
+      </button>
+    </div>
+  );
+}
+
+export default Edit;
---
+++ src/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div className="text-center text-lg font-semibold mt-5">
+      No Todo Found !
+    </div>
+  );
+}
+
+export default NotFound;
